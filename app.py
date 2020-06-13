5e377b1a13f462c238ae949eb7f6781ed1497e45
szeting <44317293+szeting@users.noreply.github.com>
2020-06-13 10:16:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -78,59 +78,5 @@ def predict():
 
     return render_template('index.html', prediction_text='The Predicted House Price is: RM {}'.format(output))
 
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    data = request.get_json(force=True)
-    data_unseen = pd.DataFrame([data])
-    def type_label (row):
-        if row['Type'] == 'Apartments':
-            return 0
-        if row['Type'] == 'Houses' :
-            return 1
-
-    def title_label (row):
-        if row['Title'] == 'Freehold':
-            return 0
-        if row['Title'] == 'Leasehold' :
-            return 1
-
-    def oth1_label (row):
-        if row['Oth_Info'] == 'Non Bumi Lot':
-            return 0
-        if row['Oth_Info'] == 'Bumi Lot':
-            return 0
-        if row['Oth_Info'] == 'Malay Reserved' :
-            return 1
-
-    def oth2_label (row):
-        if row['Oth_Info'] == 'Malay Reserved':
-            return 0
-        if row['Oth_Info'] == 'Bumi Lot':
-            return 0
-        if row['Oth_Info'] == 'Non Bumi Lot' :
-            return 1
-
-    def state_label (row):
-        if row['State'] == 'Kuala Lumpur':
-            return 0
-        if row['State'] == 'Selangor' :
-            return 1
-
-    data_unseen['Type_ Houses']=data_unseen.apply (lambda row: type_label(row), axis=1)
-    data_unseen['Title_Leasehold']=data_unseen.apply (lambda row: title_label(row), axis=1)
-    data_unseen['Oth_Info_Malay Reserved']=data_unseen.apply (lambda row: oth1_label(row), axis=1)
-    data_unseen['Oth_Info_Non Bumi Lot']=data_unseen.apply (lambda row: oth2_label(row), axis=1)
-    data_unseen['State_Selongor']=data_unseen.apply (lambda row: state_label(row), axis=1)
-
-    data_unseen=data_unseen.drop(['Type','Title','Oth_Info','State'],axis=1)
-   
-    data_unseen=data_unseen.values
-    data_unseen=scaler.transform(data_unseen.reshape(-1,9))
-    
-    prediction = model.predict(data_unseen)
-    output = int(prediction[0])
-    return jsonify(output)
-
-
 if __name__ == '__main__':
     app.run(debug=True)
