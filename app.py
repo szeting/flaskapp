9e53c9df7f879abb7f5bfd15a878333000719f82
szeting <44317293+szeting@users.noreply.github.com>
2020-06-13 09:46:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from werkzeug.wrappers import Request, Response
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, url_for, redirect
 from tensorflow.keras.models import load_model
 import pickle
 
